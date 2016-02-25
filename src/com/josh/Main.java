b93809b84aaca3a7381d5071953985fb5bd021ff
kortasthekiller <kortas@gmail.com>
2016-02-25 17:03:45 -0600

    Initial commit

---
+++ src/com/josh/Main.java
@@ -0,0 +1,13 @@
+package com.josh;
+
+public class Main {
+
+    public static void main(String[] args) {
+	boolean a = true;
+        while (a == true) {
+            System.out.println("Hello");
+        }
+
+
+    }
+}
